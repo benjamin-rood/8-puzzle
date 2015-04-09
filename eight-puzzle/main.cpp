ded2d5f05b54d4906ebc390cac37c0f62fb7ed6b
Benjamin Rood <benisrood@gmail.com>
2015-04-09 15:43:05 +1200

    Initial Commit

---
+++ eight-puzzle/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  eight-puzzle
+//
+//  Created by Benjamin Rood on 9/04/15.
+//  Copyright (c) 2015 Dave & Ben. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
