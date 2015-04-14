67b21f4841987ab7c8bda5077881df1fcd319667
Benjamin Rood <benisrood@gmail.com>
2015-04-14 15:33:19 +1200

    created priority_stack.h

---
+++ eight-puzzle/priority_stack.h
@@ -0,0 +1,24 @@
+//
+//  priority_stack.h
+//  eight-puzzle
+//
+//  Created by Benjamin Rood on 14/04/15.
+//  Copyright (c) 2015 Dave & Ben. All rights reserved.
+//
+
+#ifndef eight_puzzle_priority_stack_h
+#define eight_puzzle_priority_stack_h
+
+#include <vector>
+
+void insert( const std::vector<T>& stack, const T& obj ) {
+	std
+}
+
+T& top( const std::vector<T>& stack ) {
+	return stack.back();
+}
+
+
+
+#endif
