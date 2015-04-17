dabe62ec93199c4a2fa3da6e0b4373ede03b9307
Benjamin Rood <benisrood@gmail.com>
2015-04-17 15:53:02 +1200

    Added Max Q Length, Path Length, Num of Expansions states to printSUCCESS
    Observed:
    
    Memory requirements for PDS ~ 600KB
    
    Memory requirements for Breath First Search ~ 13MB!!

--- eight-puzzle/main.cpp
+++ eight-puzzle/main.cpp
@@ -22,19 +22,21 @@ void print_inBitSet( const std::bitset<domain_size>& set, const Board& B )	{
 	else std::cout << "NO\n";
 }
 
-void print_SUCCESS ( std::shared_ptr<Board>& B )	{
+void print_SUCCESS ( std::shared_ptr<Board>& B, size_t queueSize, uint32_t numExpansions )	{
 	std::cout << "SUCCESS!\n\n";
 	printBoard( B );
 	std::cout << std::endl;
+	std::cout << "Maximum Queue Length = " << queueSize << std::endl;
 	std::cout << "Path Length = " << getPathLength(B) << std::endl;
+	std::cout << "Number of State Expansions = " << numExpansions << std::endl;
 	std::string successPath = getMoveHistoryString( B );
 	for ( auto& s : successPath )
 		std::cout << s << " > ";
-	std::cout << "GOAL\n\n\n\n\n\n\n";
+	std::cout << "GOAL\n\n";
 }
 
 void print_FAIL ( void )	{
-	std::cout << "FAILED to find GOAL.\n\n\n\n\n";
+	std::cout << "FAILED to find GOAL.\n\n";
 }
 
 int main(int argc, const char * argv[]) {
@@ -42,6 +44,9 @@ int main(int argc, const char * argv[]) {
 	
 	// PROGRESSIVE DEEPING SEARCH:
 	{
+		bool success = false;
+		size_t highest_Q_length = 0;
+		uint32_t num_of_expansions = 0;
 		std::bitset<domain_size> visitedList;			//	the same type gets used for both visited and strict expanded list.
 														//	a bitset is initialised by default with all bits set to 0 (false).
 		
@@ -50,6 +55,8 @@ int main(int argc, const char * argv[]) {
 		
 		uint32_t depth_stop = 1;
 		uint32_t depth_max = 60;
+		
+		std::cout << "PROGRESSIVE DEEPING SEARCH\n\n";
 		
 		while ( depth_stop != depth_max ) {
 			++depth_stop;
@@ -62,12 +69,15 @@ int main(int argc, const char * argv[]) {
 			
 			
 			while (!( qStack.empty() ))	{
+				if (highest_Q_length < qStack.size() )	highest_Q_length = qStack.size();
+				
 				B = qStack.top();
 				qStack.pop();	//	once B retrieved from qStack, pop qStack.
 				visitedList.set( B->getHash() );	//	add B to visited list
 				
 				if ( testForGoalState(B) )	{
-					print_SUCCESS(B);
+					success = true;
+					print_SUCCESS(B, highest_Q_length, num_of_expansions);
 					break;
 				}
 				
@@ -80,6 +90,7 @@ int main(int argc, const char * argv[]) {
 				
 				
 				expandedStack = spawnBoardMovesFrom( B );
+				++num_of_expansions;
 				
 				while (!( expandedStack.empty() ))	{
 					auto& top = expandedStack.top();
@@ -88,65 +99,59 @@ int main(int argc, const char * argv[]) {
 					expandedStack.pop();
 				}
 			}
-			
-			if ( testForGoalState(B) )	break;
-//			else {
-//				std::cout << "Progressive Deepening Search -- goal not found.\n\n";
-//			}
-			
-			
+			//	end PDS dive at depth_stop
+			if ( success )	break;
 		}
 		
-		
+		if (!( success ))
+			print_FAIL();
 	}	//	end PDS.
 	
 	
 	// BREADTH-FIRST SEARCH:
 	{
-		 std::cout << "BREADTH-FIRST SEARCH\n\n";
-		 std::queue<std::shared_ptr<Board>> BreadthFirst_Q;	//	FIFO Enqueued List!
-		 std::stack<std::shared_ptr<Board>> expandedStack;
-		 std::bitset<domain_size> visitedList;			//	the same type gets used for both visited and strict expanded list.
-		 //	a bitset is initialised by default with all bits set to 0 (false).
-		 
-		 std::shared_ptr<Board> start = std::make_shared<Board>( start4State );
-		 BreadthFirst_Q.push( start );
-		 std::shared_ptr<Board>& B = start;
-		 
-		 
-		 while (!( BreadthFirst_Q.empty() ))	{
-			//std::cout << "Enqueued List size = " << BreadthFirst_Q.size() << std::endl;
+		std::cout << "BREADTH-FIRST SEARCH\n\n";
+		
+		size_t highest_Q_length = 0;
+		uint32_t num_of_expansions = 0;
+		
+		std::queue<std::shared_ptr<Board>> BreadthFirst_Q;	//	FIFO Enqueued List!
+		std::stack<std::shared_ptr<Board>> tempStack;
+		std::stack<std::shared_ptr<Board>> expandedStack;
+		std::bitset<domain_size> visitedList;			//	the same type gets used for both visited and strict expanded list.
+		//	a bitset is initialised by default with all bits set to 0 (false).
+
+		std::shared_ptr<Board> start = std::make_shared<Board>( start4State );
+		BreadthFirst_Q.push( start );
+		std::shared_ptr<Board>& B = start;
+
+
+		while (!( BreadthFirst_Q.empty() ))	{
+			if (highest_Q_length < BreadthFirst_Q.size() )	highest_Q_length = BreadthFirst_Q.size();
 			B = BreadthFirst_Q.front();
 			BreadthFirst_Q.pop();
-			if ( testForGoalState(B) ) break;
+			if ( testForGoalState(B) ) {
+				print_SUCCESS(B, highest_Q_length, num_of_expansions);
+				break;
+			}
 
 			visitedList.set( B->getHash() );
-			expandedStack = spawnBoardMovesFrom( B );
+			tempStack = spawnBoardMovesFrom( B );
+			++num_of_expansions;
 			
 			
+			while (!( tempStack.empty() ))	{
+				expandedStack.push( tempStack.top() );
+				tempStack.pop();
+			}
+
 			while (!( expandedStack.empty() )) {
 				auto& top = expandedStack.top();
 				if ( visitedList[top->getHash()] == false )
 					BreadthFirst_Q.push( top );
 				expandedStack.pop();
 			}
-		 }
-		
-
-		 printBoard( B );
-		 std::string successPath = getMoveHistoryString( B );
-		 
-		 for ( auto& s : successPath )
-			std::cout << s << " > ";
-		 std::cout << "GOAL\n\n\n\n\n\n\n";
-		 
-		 // empty expandedStack for next use:
-		 if (!( expandedStack.empty() )) {
-			while (!( expandedStack.empty() ))
-		 expandedStack.pop();
-		 }
-		 //	reset visited list!
-		 visitedList.reset();
+		}
 	}
 }
 
